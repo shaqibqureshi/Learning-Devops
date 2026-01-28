9a22b883334fa9f4916a94fa160d2111f70b082d
shaqib <qureshishaqib4@gmail.com>
2026-01-28 23:20:08 +0530

    not done much today can't lie to myself i have to improve

--- Bash-scripting/bash-practice.sh
+++ Bash-scripting/bash-practice.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-day - 9 
+
+#!/bin/bash
+
+: '
+day - 9
 
 read -p "Enter your age: " age
 
@@ -8,5 +12,31 @@ if [ "$age" -ge 18 ]
 then
     echo "You are 18 or above, you can enter"
 else
-    echo "You are less than 18, you can't enter"
+    echo "You are less than 18, you can"
+fi
+'
+
+# day - 10
+
+num1=55
+num2=10
+
+echo $((num1 + num2))
+
+if [ -d "config" ]
+
+then 
+	echo " reading config file directory"
+
+	for file in config/*
+	
+	do
+		echo "----$file----"
+		cat "$file"
+	done
+else
+	echo "no directory exists, creating one"
+	mkdir config 
+	touch config /file.txt
+
 fi
